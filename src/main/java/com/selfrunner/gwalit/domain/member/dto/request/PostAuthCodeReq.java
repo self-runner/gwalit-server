f2b7d65b40385b243204fbb3cc07f2db3c75efa3
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-07-12 00:29:45 +0900

    Fix: DTO Validation 추가 #10

--- src/main/java/com/selfrunner/gwalit/domain/member/dto/request/PostAuthCodeReq.java
+++ src/main/java/com/selfrunner/gwalit/domain/member/dto/request/PostAuthCodeReq.java
@@ -3,13 +3,21 @@ package com.selfrunner.gwalit.domain.member.dto.request;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @RequiredArgsConstructor
 public class PostAuthCodeReq {
 
+    @NotEmpty
+    @Size(min = 10, max = 11)
     private String phone;
 
     private String type;
 
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String authorizationCode;
 }
--- src/main/java/com/selfrunner/gwalit/domain/member/dto/request/PostAuthPhoneReq.java
+++ src/main/java/com/selfrunner/gwalit/domain/member/dto/request/PostAuthPhoneReq.java
@@ -3,9 +3,14 @@ package com.selfrunner.gwalit.domain.member.dto.request;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @RequiredArgsConstructor
 public class PostAuthPhoneReq {
 
+    @NotEmpty
+    @Size(min = 10, max = 11)
     private String phone;
 }
