ed080a62dc908ebfa756e513bb5f0ecad22e012d
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-07-25 23:44:42 +0900

    Feat: Dto 내 toEntity 생성 #21

--- src/main/java/com/selfrunner/gwalit/domain/banner/dto/request/BannerReq.java
+++ src/main/java/com/selfrunner/gwalit/domain/banner/dto/request/BannerReq.java
@@ -1,5 +1,6 @@
 package com.selfrunner.gwalit.domain.banner.dto.request;
 
+import com.selfrunner.gwalit.domain.banner.entity.Banner;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,9 +10,18 @@ import javax.validation.constraints.Pattern;
 
 @Getter
 @RequiredArgsConstructor
-public class PostBannerReq {
+public class BannerReq {
 
     @NotNull
     @Pattern(regexp = "(https?:\\/\\/)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
     private String linkUrl;
+
+    public Banner toEntity(String imageUrl) {
+        Banner banner = Banner.builder()
+                .imageUrl(imageUrl)
+                .linkUrl(this.linkUrl)
+                .build();
+
+        return banner;
+    }
 }
