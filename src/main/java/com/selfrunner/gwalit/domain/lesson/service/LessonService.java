a55eade930fc553159ddb39e4e43f96ed662333e
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-07-31 17:34:00 +0900

    Feat: Lesson 생성 API #26

--- src/main/java/com/selfrunner/gwalit/domain/lesson/service/LessonService.java
+++ src/main/java/com/selfrunner/gwalit/domain/lesson/service/LessonService.java
@@ -1,8 +1,13 @@
 package com.selfrunner.gwalit.domain.lesson.service;
 
 import com.selfrunner.gwalit.domain.lesson.dto.request.PostLessonReq;
+import com.selfrunner.gwalit.domain.lesson.entity.Lesson;
 import com.selfrunner.gwalit.domain.lesson.repository.LessonRepository;
 import com.selfrunner.gwalit.domain.member.entity.Member;
+import com.selfrunner.gwalit.domain.member.entity.MemberAndLecture;
+import com.selfrunner.gwalit.domain.member.repository.MemberAndLectureRepository;
+import com.selfrunner.gwalit.global.exception.ApplicationException;
+import com.selfrunner.gwalit.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class LessonService {
 
     private final LessonRepository lessonRepository;
+    private final MemberAndLectureRepository memberAndLectureRepository;
 
     public Void register(Member member, PostLessonReq postLessonReq) {
         // Validation
+        MemberAndLecture memberAndLecture = memberAndLectureRepository.findMemberAndLectureByMemberAndLectureLectureId(member, Long.valueOf(postLessonReq.getLectureId())).orElseThrow(() -> new ApplicationException(ErrorCode.UNAUTHORIZED_EXCEPTION));
 
         // Business Logic
+        Lesson lesson = postLessonReq.toEntity(memberAndLecture.getLecture());
+        lessonRepository.save(lesson);
 
         // Response
         return null;
