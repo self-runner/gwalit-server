d98f2dbb5be733f9ebd7426ee44e448fd4507e92
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-08-22 02:31:58 +0900

    Chore: Sentry 모니터링 시, src ip 포함하도록 추가 #83

--- src/main/java/com/selfrunner/gwalit/global/config/WebConfig.java
+++ src/main/java/com/selfrunner/gwalit/global/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.selfrunner.gwalit.global.config;
 
+import com.selfrunner.gwalit.global.logging.SentryInterceptor;
 import com.selfrunner.gwalit.global.util.jwt.AuthAuthorizationArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> handlerMethodArgumentResolverList) {
         handlerMethodArgumentResolverList.add(authAuthorizationArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SentryInterceptor());
     }
 }
