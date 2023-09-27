e56983e1da4c496381986fa80d5c02386da65432
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-09-27 15:18:05 +0900

    Feat: 문제생성요청 Dto #138

--- src/main/java/com/selfrunner/gwalit/domain/workbook/dto/request/PostProblemReq.java
+++ src/main/java/com/selfrunner/gwalit/domain/workbook/dto/request/PostProblemReq.java
@@ -1,4 +1,32 @@
-행package com.selfrunner.gwalit.domain.workbook.dto.request;
+package com.selfrunner.gwalit.domain.workbook.dto.request;
 
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Getter
 public class PostProblemReq {
+
+    @Pattern(regexp = "")
+    private String subject;
+
+    @Pattern(regexp = "")
+    private String subjectDetail;
+
+    @Pattern(regexp = "")
+    private String type;
+
+    private String body;
+
+    @NotNull
+    private Integer answer;
+
+    private String solveBody;
+
+    @NotNull
+    private String source;
+
+    @Pattern(regexp = "")
+    private String difficulty;
 }
