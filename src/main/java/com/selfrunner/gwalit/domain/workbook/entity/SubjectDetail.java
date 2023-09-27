619a32c03f94a6b48f410adf5d799406f72ecd70
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-09-27 15:17:48 +0900

    Feat: SubjectDetail에 수학/사회탐구 과목별 정보 추가 #138

--- src/main/java/com/selfrunner/gwalit/domain/workbook/entity/SubjectDetail.java
+++ src/main/java/com/selfrunner/gwalit/domain/workbook/entity/SubjectDetail.java
@@ -1,9 +1,10 @@
 package com.selfrunner.gwalit.domain.workbook.entity;
 
 public enum SubjectDetail {
-    // 수학
+    // 수학 (수학1, 수학2, 미적분, 확률과 통계, 기하)
+    math1, math2, calculus, statistics, geometry,
 
-
-    // 사회탐구
+    // 사회탐구 (사회문화, 생활과 윤리, 윤리와 사상, 법과 정치)
+    society_culture, everyday_ethics, ehtics_ideology, law_politics
 
 }
