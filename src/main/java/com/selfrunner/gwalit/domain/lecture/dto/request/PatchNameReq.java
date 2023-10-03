f7f711f9b39511bbcd425f551d983cbb5459993e
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-03 13:48:03 +0900

    Fix: 클래스 이름 글자 수 조건 추가 #177

--- src/main/java/com/selfrunner/gwalit/domain/lecture/dto/request/PatchNameReq.java
+++ src/main/java/com/selfrunner/gwalit/domain/lecture/dto/request/PatchNameReq.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @RequiredArgsConstructor
 public class PatchNameReq {
 
     @NotEmpty(message = "Class 이름이 공란입니다.")
+    @Size(min = 2, max = 16, message = "클래스명은 2자 ~ 16자 사이여야 합니다.")
     private String name;
 }
