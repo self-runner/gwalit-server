e4cdf239fc5428e8f0a3517f76af732b3e5da631
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-09 03:29:00 +0900

    Fix: 문제집 정보용 Enum에 이름 정보 포함되도록 수정 #138

--- src/main/java/com/selfrunner/gwalit/domain/workbook/entity/WorkbookType.java
+++ src/main/java/com/selfrunner/gwalit/domain/workbook/entity/WorkbookType.java
@@ -1,8 +1,27 @@
 package com.selfrunner.gwalit.domain.workbook.entity;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public enum WorkbookType {
-    // 객관식
-    MULTIPLE,
+    // 모의고사
+    MOCKEXAM("모의고사", "mockexam"),
+
+    // 단원평가
+    CHAPTEREXAM("단원 평가", "chapterexam"),
+
     // OX퀴즈
-    TFQUIZ
+    SHORTEXAM("1분 퀴즈", "shortexam");
+
+    private final String koreanName;
+
+    private final String englishName;
+
+    WorkbookType(String koreanName, String englishName) {
+        this.koreanName = koreanName;
+        this.englishName = englishName;
+    }
 }
+ㅌ
