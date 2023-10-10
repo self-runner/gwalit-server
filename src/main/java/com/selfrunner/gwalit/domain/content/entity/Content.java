86c8997ee9bcdf366259ba23a76a65dfe18fa42c
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-10 14:03:37 +0900

    Feat: Content Entity 변경된 설계 적용 #194

--- src/main/java/com/selfrunner/gwalit/domain/content/entity/Content.java
+++ src/main/java/com/selfrunner/gwalit/domain/content/entity/Content.java
@@ -1,7 +1,5 @@
 package com.selfrunner.gwalit.domain.content.entity;
 
-import com.selfrunner.gwalit.domain.content.dto.request.PutContentReq;
-import com.selfrunner.gwalit.domain.member.dto.request.PutMemberReq;
 import com.selfrunner.gwalit.global.common.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -11,7 +9,6 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
-import java.sql.Time;
 
 @Entity
 @Getter
@@ -41,11 +38,11 @@ public class Content extends BaseTimeEntity {
     private ContentClassification classification;
 
 
-    @Column(name = "url_link", columnDefinition = "text")
-    private String urlLink;
+    @Column(name = "link_url", columnDefinition = "text")
+    private String linkUrl;
 
-    @Column(name = "thumbnail_link", columnDefinition = "text")
-    private String thumbnailLink;
+    @Column(name = "thumbnail_url", columnDefinition = "text")
+    private String thumbnailUrl;
 
     @Column(name = "duration", columnDefinition = "int")
     private Integer duration;
@@ -56,19 +53,19 @@ public class Content extends BaseTimeEntity {
     public void update(Content content) {
         this.title = content.getTitle();
         this.type = content.getType();
-        this.urlLink = content.getUrlLink();
-        this.thumbnailLink = content.getThumbnailLink();
+        this.linkUrl = content.getLinkUrl();
+        this.thumbnailUrl = content.getThumbnailUrl();
         this.duration = content.getDuration();
     }
 
     @Builder
-    public Content(String title, String writer, String type, String classification, String urlLink, String thumbnailLink, Integer duration, Boolean isPinned) {
+    public Content(String title, String writer, String type, String classification, String linkUrl, String thumbnailUrl, Integer duration, Boolean isPinned) {
         this.title = title;
         this.writer = writer;
         this.type = ContentType.valueOf(type);
         this.classification = ContentClassification.valueOf(classification);
-        this.urlLink = urlLink;
-        this.thumbnailLink = thumbnailLink;
+        this.linkUrl = linkUrl;
+        this.thumbnailUrl = thumbnailUrl;
         this.duration = duration;
         this.isPinned = isPinned;
     }
