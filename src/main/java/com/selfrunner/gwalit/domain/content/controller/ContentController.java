7b31d1b1044b3304c7702693cdb53b631333aecb
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-10 14:04:08 +0900

    Feat: Content 등록 API #194

--- src/main/java/com/selfrunner/gwalit/domain/content/controller/ContentController.java
+++ src/main/java/com/selfrunner/gwalit/domain/content/controller/ContentController.java
@@ -4,15 +4,19 @@ import com.selfrunner.gwalit.domain.content.dto.request.ContentReq;
 import com.selfrunner.gwalit.domain.content.dto.request.PutContentReq;
 import com.selfrunner.gwalit.domain.content.dto.response.ContentRes;
 import com.selfrunner.gwalit.domain.content.service.ContentService;
+import com.selfrunner.gwalit.domain.member.entity.Member;
 import com.selfrunner.gwalit.global.common.ApplicationResponse;
 import com.selfrunner.gwalit.global.exception.ErrorCode;
+import com.selfrunner.gwalit.global.util.jwt.Auth;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -24,13 +28,8 @@ public class ContentController {
     private final ContentService contentService;
 
     @PostMapping("")
-    public ApplicationResponse<Void> register(@Valid @RequestBody ContentReq contentReq) {
-        return ApplicationResponse.create(ErrorCode.SUCCESS, contentService.register(contentReq));
-    }
-
-    @GetMapping("")
-    public ApplicationResponse<List<ContentRes>> getAll() {
-        return ApplicationResponse.ok(ErrorCode.SUCCESS, contentService.getAll());
+    public ApplicationResponse<ContentRes> register(@Auth Member member, @Valid @RequestPart(value = "data") ContentReq contentReq, @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnailImage) {
+        return ApplicationResponse.create(ErrorCode.SUCCESS, contentService.register(member, contentReq, thumbnailImage));
     }
 
     @PutMapping("/{content_id}")
@@ -42,4 +41,10 @@ public class ContentController {
     public ApplicationResponse<Void> delete(@PathVariable("content_id") Long contentId) {
         return ApplicationResponse.ok(ErrorCode.SUCCESS, contentService.delete(contentId));
     }
+
+    @GetMapping("")
+    public ApplicationResponse<List<ContentRes>> getAll() {
+        return ApplicationResponse.ok(ErrorCode.SUCCESS, contentService.getAll());
+    }
+
 }
--- src/main/java/com/selfrunner/gwalit/domain/content/service/ContentService.java
+++ src/main/java/com/selfrunner/gwalit/domain/content/service/ContentService.java
@@ -1,15 +1,19 @@
 package com.selfrunner.gwalit.domain.content.service;
 
 import com.selfrunner.gwalit.domain.content.dto.request.ContentReq;
-import com.selfrunner.gwalit.domain.content.dto.request.PutContentReq;
 import com.selfrunner.gwalit.domain.content.dto.response.ContentRes;
 import com.selfrunner.gwalit.domain.content.entity.Content;
+import com.selfrunner.gwalit.domain.content.entity.ContentType;
+import com.selfrunner.gwalit.domain.content.exception.ContentException;
 import com.selfrunner.gwalit.domain.content.repository.ContentRepository;
+import com.selfrunner.gwalit.domain.member.entity.Member;
 import com.selfrunner.gwalit.global.exception.ApplicationException;
 import com.selfrunner.gwalit.global.exception.ErrorCode;
+import com.selfrunner.gwalit.global.util.aws.S3Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,17 +24,70 @@ import java.util.stream.Collectors;
 public class ContentService {
 
     private final ContentRepository contentRepository;
+    private final S3Client s3Client;
 
     @Transactional
-    public Void register(ContentReq contentReq) {
+    public ContentRes register(Member member, ContentReq contentReq, MultipartFile thumbnailImage) {
         // Validation
+        /**
+         * TODO: 관리자 권한 확인 조건 추가 필요
+         */
+        if(contentRepository.existsByLinkUrl(contentReq.getLinkUrl())) {
+            throw new ContentException(ErrorCode.ALREADY_EXIST_CONTENT);
+        }
+
+        // Business Logic: 비디오인지, 노션인지 따라에 구분된 로직 진행
+        switch (ContentType.valueOf(contentReq.getType())) {
+            case VIDEO:
+                Content videoContent = contentReq.toEntity(null);
+                contentRepository.save(videoContent);
+
+                return new ContentRes(videoContent);
+
+            case NOTION:
+                String thumbnailUrl;
+                try {
+                    thumbnailUrl = s3Client.upload(thumbnailImage, "content");
+
+                } catch (Exception e) {
+                    throw new ContentException(ErrorCode.INTERNAL_SERVER_EXCEPTION);
+                }
+
+                Content notionContent = contentReq.toEntity(thumbnailUrl);
+                contentRepository.save(notionContent);
+
+                return new ContentRes(notionContent);
+        }
+
+        // Response
+        return null;
+    }
+
+    @Transactional
+    public ContentRes update(Long contentId, ContentReq contentReq) {
+        // Validation
+        Content content = contentRepository.findById(contentId).orElseThrow();
         if(contentRepository.existsByLinkUrl(contentReq.getLinkUrl())) {
             throw new ApplicationException(ErrorCode.ALREADY_EXIST_CONTENT);
         }
 
         // Business Logic
-        Content content = contentReq.toEntity();
-        contentRepository.save(content);
+        //content.update(contentReq.toEntity());
+
+        // Response
+        return new ContentRes(content);
+    }
+
+    @Transactional
+    public Void delete(Long contentId) {
+        // Validation
+        Content content = contentRepository.findById(contentId).orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_EXCEPTION));
+        if(content.getDeletedAt() != null) {
+            throw new ApplicationException(ErrorCode.ALREADY_DELETE_EXCEPTION);
+        }
+
+        // Business Logic
+        contentRepository.delete(content);
 
         // Response
         return null;
@@ -47,36 +104,5 @@ public class ContentService {
         return contents.stream()
                 .map(ContentRes::new)
                 .collect(Collectors.toList());
-    }
-
-    @Transactional
-    public ContentRes update(Long contentId, ContentReq contentReq) {
-        // Validation
-        Content content = contentRepository.findById(contentId).orElseThrow();
-        if(contentRepository.existsByLinkUrl(contentReq.getLinkUrl())) {
-            throw new ApplicationException(ErrorCode.ALREADY_EXIST_CONTENT);
-        }
-
-        // Business Logic
-        content.update(contentReq.toEntity());
-
-        // Response
-        ContentRes contentRes = new ContentRes(content);
-        return contentRes;
-    }
-
-    @Transactional
-    public Void delete(Long contentId) {
-        // Validation
-        Content content = contentRepository.findById(contentId).orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_EXCEPTION));
-        if(content.getDeletedAt() != null) {
-            throw new ApplicationException(ErrorCode.ALREADY_DELETE_EXCEPTION);
-        }
-
-        // Business Logic
-        contentRepository.delete(content);
-
-        // Response
-        return null;
     }
 }
