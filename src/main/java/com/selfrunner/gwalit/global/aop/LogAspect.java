5df198357b77b65237b1d007f3033d382cbe7ce4
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-10-17 01:43:36 +0900

    Chore: 비즈니스 로직별 실행 시간 측정 및 1초 이상일 경우에 한해 Sentry로 message 보내도록 설정 #190

--- src/main/java/com/selfrunner/gwalit/global/aop/LogAspect.java
+++ src/main/java/com/selfrunner/gwalit/global/aop/LogAspect.java
@@ -1,33 +1,46 @@
 package com.selfrunner.gwalit.global.aop;
 
 import com.selfrunner.gwalit.global.exception.ApplicationException;
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LogAspect {
-
-    // 호출된 API 정보 기록
-    @Before("com.selfrunner.gwalit.global.aop.Pointcuts.allController()")
-    public Object apiLog(ProceedingJoinPoint joinPoint) throws Throwable {
-
-    }
 
     // 비즈니스 로직의 실행시간 기록
     @Around("com.selfrunner.gwalit.global.aop.Pointcuts.allService()")
     public Object executionTimeLog(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
+            Long startTime = System.currentTimeMillis();
+            String method = joinPoint.getSignature().toString();
             Object result = joinPoint.proceed();
+
+            Long endTime = System.currentTimeMillis();
+            long executionTime = endTime - startTime;
+
+            if(executionTime < 1000) {
+                log.info("[" + method + "] " + " execution time: " + executionTime + "ms");
+            }
+            else {
+                log.warn("[" + method + "] " + " execution time: " + executionTime + "ms");
+                Sentry.captureMessage("Slow Query: " + "[" + method + "] " + " execution time: " + executionTime + "ms");
+            }
 
             return result;
         } catch (ApplicationException e) {
-
+            String method = joinPoint.getSignature().toString();
+            log.error("[" + method + "] " + " Application Exception: " + e.getErrorCode().toString());
+            throw e;
         } catch (Exception e) {
-
+            String method = joinPoint.getSignature().toString();
+            log.error("[" + method + "] " + " Application Exception: " + e.getMessage());
+            throw e;
         }
     }
 }
