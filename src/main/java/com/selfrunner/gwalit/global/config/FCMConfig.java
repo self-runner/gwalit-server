59e6c95566fce3a8ea2b1e5560911bfed464c075
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-11-02 15:10:20 +0900

    Chore: FCM 설정 업데이트 #165

--- src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
+++ src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
@@ -1,7 +1,37 @@
 package com.selfrunner.gwalit.global.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Configuration
 public class FCMConfig {
+
+    @Value("${firebase.config}")
+    private String configFile;
+
+    @Value("${firebase.project.id}")
+    private String projectId;
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(configFile);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if(FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseMessaging.getInstance();
+    }
 }
