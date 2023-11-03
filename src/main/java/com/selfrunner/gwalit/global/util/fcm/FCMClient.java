13085fb9c1bcca691396f3e94eb0dcc207df3a37
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-11-04 03:48:14 +0900

    Feat: FCM 송신 메소드 개발 #165

--- src/main/java/com/selfrunner/gwalit/global/util/fcm/FCMClient.java
+++ src/main/java/com/selfrunner/gwalit/global/util/fcm/FCMClient.java
@@ -1,18 +1,25 @@
 package com.selfrunner.gwalit.global.util.fcm;
 
+import com.google.firebase.messaging.*;
+import com.selfrunner.gwalit.global.exception.ApplicationException;
+import com.selfrunner.gwalit.global.exception.ErrorCode;
+import com.selfrunner.gwalit.global.util.fcm.dto.FCMMessageDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FCMClient {
 
-    public String sendNotification(String content, String token) {
+    public String send(FCMMessageDto fcmMessageDto) {
         try {
-            Message message = Message.builder()
-                    .setToken(token)
-                    .putData("title", content)
-                    .build();
+            Message message = makeMessage(fcmMessageDto);
 
             String response = FirebaseMessaging.getInstance().send(message);
             // return response if firebase messaging is successfully completed.
@@ -20,25 +27,64 @@ public class FCMClient {
 
         } catch (FirebaseMessagingException e) {
             e.printStackTrace();
-            return "Failed";
+            throw new ApplicationException(ErrorCode.FAILED_SEND_MESSAGE);
         }
     }
 
-    public void sendMessageTo(String targetToken, String title, String body, String id, String isEnd) throws IOException {
-        String message = makeMessage(targetToken, title, body, id, isEnd);
+    public void sendAll(List<FCMMessageDto> fcmMessageDtoList) {
+        BatchResponse response;
+        try {
+            List<Message> messageList = fcmMessageDtoList.stream()
+                    .map(this::makeMessage)
+                    .collect(Collectors.toList());
 
-        OkHttpClient client = new OkHttpClient();
-        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+            // 알림 발송
+            response = FirebaseMessaging.getInstance().sendAll(messageList);
 
-        Request request = new Request.Builder()
-                .url(FIREBASE_ALARM_SEND_API_URI)
-                .post(requestBody)
-                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer "+getAccessToken())
-                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+            // 요청에 대한 응답 처리
+            if (response.getFailureCount() > 0) {
+                List<SendResponse> responses = response.getResponses();
+                List<String> failedTokens = new ArrayList<>();
+
+                for (int i = 0; i < responses.size(); i++) {
+                    if (!responses.get(i).isSuccessful()) {
+                        failedTokens.add(fcmMessageDtoList.get(i).getToken());
+                    }
+                }
+                log.error("List of tokens are not valid FCM token : " + failedTokens);
+            }
+        } catch (FirebaseMessagingException e) {
+            log.error("cannot send to memberList push message. error info : {}", e.getMessage());
+        }
+    }
+
+    private Message makeMessage(FCMMessageDto fcmMessageDto) {
+        // 딥링크용 정보가 없을 경우
+        if(fcmMessageDto.getData() == null) {
+            return Message.builder()
+                    .setToken(fcmMessageDto.getToken())
+                    .setNotification(Notification.builder()
+                            .setTitle(fcmMessageDto.getNotification().getTitle())
+                            .setBody(fcmMessageDto.getNotification().getBody())
+                            .build())
+                    .setAndroidConfig(AndroidConfig.builder()
+                            .setPriority(AndroidConfig.Priority.HIGH)
+                            .build()) // Priority High 설정
+                    .build();
+        }
+
+        // 딥링크용 정보가 있는 경우
+        return Message.builder()
+                .setToken(fcmMessageDto.getToken())
+                .setNotification(Notification.builder()
+                        .setTitle(fcmMessageDto.getNotification().getTitle())
+                        .setBody(fcmMessageDto.getNotification().getBody())
+                        .build())
+                .putData("name", fcmMessageDto.getData().getName())
+                .putData("params", fcmMessageDto.getData().getParams().toString())
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setPriority(AndroidConfig.Priority.HIGH)
+                        .build()) // Priority High 설정
                 .build();
-
-        Response response = client.newCall(request).execute();
-
-        log.info(response.body().string());
     }
 }
