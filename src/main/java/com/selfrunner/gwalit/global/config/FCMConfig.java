a4d32106750fde2f40cb7a35fe3db2d9991e6ae3
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-11-04 03:48:42 +0900

    Chore: FCM 환경설정 업데이트 #165

--- src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
+++ src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
@@ -4,23 +4,28 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.selfrunner.gwalit.global.exception.ApplicationException;
+import com.selfrunner.gwalit.global.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 public class FCMConfig {
 
     @Value("${firebase.config}")
     private String configFile;
 
-    @Value("${firebase.project.id}")
-    private String projectId;
+    @Value("${firebase.scope}")
+    private String scope;
 
     @PostConstruct
     public void initialize() {
@@ -29,12 +34,17 @@ public class FCMConfig {
             ClassPathResource resource =  new ClassPathResource(configFile);
             FileInputStream serviceAccount = new FileInputStream(resource.getFile());
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount).createScoped(scope))
                     .build();
-            FirebaseApp.initializeApp(options);
 
+            if(FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("FCM Initialize");
+            }
         } catch (Exception e) {
             e.printStackTrace();
+            log.error("FCM 환경 설정 오류 발생");
+            throw new ApplicationException(ErrorCode.FAILED_FCM_INIT);
         }
     }
 }
