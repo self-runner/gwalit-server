88c66dbfeb608ebd0c67964dc3cd8f24e9e03ef6
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-11-03 15:29:41 +0900

    Chore: FCM 환경설정 진행 #165

--- src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
+++ src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
@@ -7,7 +7,9 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -20,18 +22,19 @@ public class FCMConfig {
     @Value("${firebase.project.id}")
     private String projectId;
 
-    @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(configFile);
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-        if(FirebaseApp.getApps().isEmpty()) {
+    @PostConstruct
+    public void initialize() {
+        try {
+            // Service Account를 이용하여 Fireabse Admin SDK 초기화
+            ClassPathResource resource =  new ClassPathResource(configFile);
+            FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
             FirebaseApp.initializeApp(options);
-        }
 
-        return FirebaseMessaging.getInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
