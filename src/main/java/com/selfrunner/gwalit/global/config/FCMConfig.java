ad33306c5441aa2a725142e90b3bda00a3d63740
Jeongjin Lee <sso07012@mail.hongik.ac.kr>
2023-11-05 12:21:58 +0900

    Feat: 알림 테스트용 API (#274)
    * Feat: 배포환경에서의 FCM 오류 확인 #165

--- src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
+++ src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
@@ -4,6 +4,8 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import com.selfrunner.gwalit.global.exception.ApplicationException;
 import com.selfrunner.gwalit.global.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +16,11 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -29,29 +33,21 @@ public class FCMConfig {
     @Value("${firebase.scope}")
     private String scope;
 
+    @Value("${firebase.config-file}")
+    private String serializedJson;
+
     @PostConstruct
     public void initialize() {
         try {
+            // 문자열을 바이트 배열로 변환
+            byte[] byteArray = serializedJson.getBytes(StandardCharsets.UTF_8);
+
+            // ByteArrayInputStream을 사용하여 바이트 배열을 InputStream으로 변환
+            InputStream inputStream = new ByteArrayInputStream(byteArray);
+
             // Service Account를 이용하여 Fireabse Admin SDK 초기화
-            System.out.println("Test2");
-            InputStream serviceAccount = new ClassPathResource(configFile).getInputStream();
-            InputStream serviceAccountStream = new ClassPathResource(configFile).getInputStream();
-
-            // InputStream에서 데이터를 읽어오기 위한 바이트 배열
-            byte[] data = new byte[1024]; // 적절한 크기로 설정
-
-            int bytesRead;
-            StringBuilder content = new StringBuilder();
-            while ((bytesRead = serviceAccountStream.read(data)) != -1) {
-                content.append(new String(data, 0, bytesRead));
-            }
-
-            String serviceAccountData = content.toString();
-            serviceAccountStream.close();
-
-            System.out.println(serviceAccountData);
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(inputStream).createScoped(List.of(scope)))
                     .build();
             System.out.println("Test4");
 
