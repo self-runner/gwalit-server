398556d9b3837aebe1e8e66ef8ec1d029b214a12
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-11-05 18:51:02 +0900

    Feat: FCMClient 메소드 구조 변경 및 관련 로직 수정 #165

--- src/main/java/com/selfrunner/gwalit/global/util/fcm/dto/FCMMessageDto.java
+++ src/main/java/com/selfrunner/gwalit/global/util/fcm/dto/FCMMessageDto.java
@@ -1,6 +1,5 @@
 package com.selfrunner.gwalit.global.util.fcm.dto;
 
-import com.selfrunner.gwalit.domain.notification.entity.Notification;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,31 +34,6 @@ public class FCMMessageDto {
     }
 
     /**
-     * 클래스 초대 시 알람 객체 만드는 메소드
-     * @param token - FCM 토큰 정보
-     * @param teacherName - 선생님 이름
-     * @param lectureName - 클래스 이름
-     * @param lectureId- 클래스 ID
-     * @return - 만들어진 FCMMessageDto 반환
-     */
-    public static FCMMessageDto toDto(String token, String teacherName, String lectureName, Long lectureId) {
-        Map<String, String> params = new HashMap<>();
-        params.put("lectureId", lectureId.toString());
-
-        return FCMMessageDto.builder()
-                .token(token)
-                .notification(FCMMessageDto.Notification.builder()
-                        .title(lectureName + "클래스 초대")
-                        .body("[과릿] " + teacherName + " 선생님으로부터 " + lectureName + " 클래스 초대가 도착했습니다." + "\n" + "접속하여 초대된 클래스를 확인해보세요!")
-                        .build())
-                .data(Data.builder()
-                        .name("studentLectureMain")
-                        .params(params)
-                        .build())
-                .build();
-    }
-
-    /**
      * 수업 리포트 등록/수정 시 알람 객체 만드는 메소드
      * @param token - FCM 토큰 정보
      * @param lectureId - 클래스 ID
@@ -88,9 +62,9 @@ public class FCMMessageDto {
 
     public static FCMMessageDto toDto(com.selfrunner.gwalit.domain.notification.entity.Notification notification) {
         Map<String, String> params = new HashMap<>();
-        params.put("lectureId", notification.getLectureId().toString());
-        params.put("lessonId", notification.getLessonId().toString());
-        params.put("date", notification.getDate().toString());
+        params.put("lectureId", (notification.getLectureId() != null) ? notification.getLectureId().toString() : null);
+        params.put("lessonId", (notification.getLessonId() != null) ? notification.getLessonId().toString() : null);
+        params.put("date", (notification.getDate() != null) ? notification.getDate().toString() : null);
         params.put("url", notification.getUrl());
 
         return FCMMessageDto.builder()
@@ -105,32 +79,3 @@ public class FCMMessageDto {
                 .build();
     }
 }
-
-/*
-{
-  "message":{
-    "topic":"subscriber-updates",
-    "notification":{
-      "body" : "This week's edition is now available.",
-      "title" : "NewsMagazine.com",
-    },
-    "data" : {
-      "volume" : "3.21.15",
-      "contents" : "http://www.news-magazine.com/world-week/21659772"
-    },
-    "android":{
-      "priority":"normal"
-    },
-    "apns":{
-      "headers":{
-        "apns-priority":"5"
-      }
-    },
-    "webpush": {
-      "headers": {
-        "Urgency": "high"
-      }
-    }
-  }
-}
- */
