d1f1548a9548ae1f8cc6a39c1b0c8d885342110e
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-11-06 22:00:59 +0900

    Feat: 배치용 Dto에 토큰 정보 포함 #281

--- src/main/java/com/selfrunner/gwalit/global/batch/dto/BatchNotificationDto.java
+++ src/main/java/com/selfrunner/gwalit/global/batch/dto/BatchNotificationDto.java
@@ -11,6 +11,8 @@ public class BatchNotificationDto {
 
     private final Long memberId; // memberAndLecture의 memberId와 바인딩
 
+    private final String token; // FCM용 토큰 포함
+
     private final List<BatchLessonDto> lessonList;
 
 }
