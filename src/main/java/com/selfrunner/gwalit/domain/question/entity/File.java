c44684158826b3f5ba6407f0f971c1c0afc9b3eb
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-12-06 12:45:33 +0900

    Feat: File Entity #351

---
+++ src/main/java/com/selfrunner/gwalit/domain/question/entity/File.java
@@ -0,0 +1,39 @@
+package com.selfrunner.gwalit.domain.question.entity;
+
+import com.selfrunner.gwalit.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "file")
+@SQLDelete(sql = "")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class File extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id", columnDefinition = "bigint")
+    private Long fileId;
+
+    private String name;
+
+    private String fileUrl;
+
+    private Long questionId;
+
+    private Long replyId;
+
+    @Builder
+    public File(String name, String fileUrl, Long questionId, Long replyId) {
+        this.name = name;
+        this.fileUrl = fileUrl;
+        this.questionId = questionId;
+        this.replyId = replyId;
+    }
+}
