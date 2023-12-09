c395b3905f652ccf64f8924346ce2f4cfeb3c1c4
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-12-09 17:21:52 +0900

    Fix: 미사용 파일 삭제 & Board 관련 Exception 분리 #351

--- src/main/java/com/selfrunner/gwalit/domain/question/repository/QuestionRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.selfrunner.gwalit.domain.question.repository;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-@RequiredArgsConstructor
-public class QuestionRepositoryImpl implements QuestionRepositoryCustom{
-}
