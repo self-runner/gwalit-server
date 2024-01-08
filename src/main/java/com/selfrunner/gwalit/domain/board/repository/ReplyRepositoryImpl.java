479a2ff593d8815c13348875689dedaceb0c4403
Jeongjin Lee <sso07012@mail.hongik.ac.kr>
2024-01-09 01:02:14 +0900

    Fix: 댓글 페이지네이션 쿼리 오류 수정 (#386)
    * Chore: DEV 환경변수는 로컬이 되도록 변경 #381
    
    * Fix: 댓글 페이지네이션 쿼리 수정 #381
    
    * Chore: 댓글 페이지네이션 쿼리 수정 #381

--- src/main/java/com/selfrunner/gwalit/domain/board/repository/ReplyRepositoryImpl.java
+++ src/main/java/com/selfrunner/gwalit/domain/board/repository/ReplyRepositoryImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.querydsl.core.group.GroupBy.groupBy;
 import static com.querydsl.core.types.Projections.list;
@@ -35,14 +36,25 @@ public class ReplyRepositoryImpl implements ReplyRepositoryCustom{
     }
 
     public Slice<ReplyRes> findReplyPaginationByBoardId(Long boardId, Long cursor, LocalDateTime cursorCreatedAt, Pageable pageable) {
-        List<ReplyRes> content = queryFactory.selectFrom(reply)
+        List<ReplyRes> replyResList = queryFactory.selectFrom(reply)
                 .leftJoin(member).on(reply.member.memberId.eq(member.memberId))
                 .leftJoin(file).on(reply.replyId.eq(file.replyId))
                 .where(reply.board.boardId.eq(boardId), eqCursorIdAndCursorCreatedAt(cursor, cursorCreatedAt), reply.deletedAt.isNull())
-                .orderBy(reply.createdAt.asc(), reply.replyId.asc())
+                .orderBy(reply.createdAt.asc(), reply.replyId.asc(), file.fileId.asc())
                 .limit(pageable.getPageSize() + 1)
-                .transform(groupBy(reply.replyId).list(Projections.constructor(ReplyRes.class, reply.replyId, reply.board.boardId, member.memberId, member.type, member.name, reply.body,
+                .transform(groupBy(reply.replyId, file.replyId)
+                    .list(Projections.constructor(ReplyRes.class, reply.replyId, reply.board.boardId, member.memberId, member.type, member.name, reply.body,
                         list(Projections.constructor(FileRes.class, file.name, file.url, file.size)), reply.createdAt, reply.modifiedAt)));
+
+        List<ReplyRes> content = replyResList.stream()
+            .map(replyRes ->  {
+                List<FileRes> fileResList = queryFactory.selectFrom(file)
+                        .where(file.replyId.eq(replyRes.getReplyId()))
+                        .transform(groupBy(file.fileId).list(Projections.constructor(FileRes.class, file.name, file.url, file.size)));
+
+                return new ReplyRes(replyRes.getReplyId(), replyRes.getBoardId(), replyRes.getMemberId(), replyRes.getMemberType(), replyRes.getMemberName(), replyRes.getBody(), fileResList, replyRes.getCreatedAt(), replyRes.getModifiedAt());
+            })
+            .collect(Collectors.toList());
 
         // 다음 페이지 존재 여부 확인
         boolean hasNext = false;
