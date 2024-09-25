5fedfeb84de28245225368d08b0f00c6ab2eeddf
dl-00-e8 <sso07012@gmail.com>
2024-09-25 23:30:49 +0900

    Feat: Redis 헬스체크 메소드 (#445)

--- src/main/java/com/selfrunner/gwalit/global/util/redis/RedisClient.java
+++ src/main/java/com/selfrunner/gwalit/global/util/redis/RedisClient.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,20 +14,46 @@ public class RedisClient {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    // Redis에 key-value 삽입
+    /**
+     * Redis에 key-value 저장
+     * @param key 저장할 key
+     * @param value 저장할 value
+     * @param timeout 데이터 유효시간 (expire time)
+     */
     public void setValue(String key, String value, Long timeout) {
         ValueOperations<String, String> values = redisTemplate.opsForValue();
         values.set(key, value, Duration.ofMinutes(timeout));
     }
 
-    // Redis에서 key로 value 반환
+    /**
+     * Redis에서 key로 value 조회
+     * @param key 조회할 key
+     * @return key에 해당하는 value
+     */
     public String getValue(String key) {
         ValueOperations<String, String> values = redisTemplate.opsForValue();
         return values.get(key);
     }
 
-    // Redis에서 key-value 삭제
+    /**
+     * Redis에서 key로 value 삭제
+     * @param key 삭제할 key
+     */
     public void deleteValue(String key) {
         redisTemplate.delete(key);
+    }
+
+    /**
+     * Redis 서버가 정상적으로 동작하는지 확인
+     * @return Redis 서버 동작 여부
+     */
+    public boolean isRedisAvailable() {
+        try {
+            return Optional.ofNullable(redisTemplate.getConnectionFactory())
+                    .map(connectionFactory -> connectionFactory.getConnection().ping() != null)
+                    .orElse(false);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
