c6d084ed117f144391a1832d250d89c6816678a5
dl-00-e8 <sso07012@gmail.com>
2024-10-30 19:00:53 +0900

    Refactor: FCM 서비스 계정 적용 방식 변경에 따른 업데이트 (#458)

--- src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
+++ src/main/java/com/selfrunner/gwalit/global/config/FCMConfig.java
@@ -8,6 +8,7 @@ import com.selfrunner.gwalit.global.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
@@ -19,26 +20,29 @@ import java.util.List;
 @Configuration
 public class FCMConfig {
 
-    //@Value("${firebase.config}")
-    //private String configFile;
+    @Value("${firebase.config}")
+    private String configFile;
 
     @Value("${firebase.scope}")
     private String scope;
 
-    @Value("${firebase.config-file}")
-    private String serializedJson;
+//    @Value("${firebase.config-file}")
+//    private String serializedJson;
 
     @PostConstruct
     public void initialize() {
         try {
-            // 문자열을 바이트 배열로 변환
-            byte[] byteArray = serializedJson.getBytes(StandardCharsets.UTF_8);
+//            // 문자열을 바이트 배열로 변환
+//            byte[] byteArray = serializedJson.getBytes(StandardCharsets.UTF_8);
+//
+//            // ByteArrayInputStream을 사용하여 바이트 배열을 InputStream으로 변환
+//            InputStream inputStream = new ByteArrayInputStream(byteArray);
 
-            // ByteArrayInputStream을 사용하여 바이트 배열을 InputStream으로 변환
-            InputStream inputStream = new ByteArrayInputStream(byteArray);
+            ClassPathResource resource = new ClassPathResource(configFile);
+            InputStream inputStream = resource.getInputStream();
 
             // Service Account를 이용하여 Fireabse Admin SDK 초기화
-            FirebaseOptions options = new FirebaseOptions.Builder()
+            FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(inputStream).createScoped(List.of(scope)))
                     .build();
 
@@ -47,8 +51,7 @@ public class FCMConfig {
                 log.info("FCM Initialize");
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            log.error("FCM 환경 설정 오류 발생");
+            log.error("FCM Initialize Error: {}", e.getMessage());
             throw new ApplicationException(ErrorCode.FAILED_FCM_INIT);
         }
     }
