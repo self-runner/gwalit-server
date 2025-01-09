ca6afbc0a92161891c4531316082c094afa407a3
dl-00-e8 <sso07012@gmail.com>
2025-01-10 01:44:19 +0900

    Fix: AOP Pointcut 경로 수정 (#449)

--- api-module/src/main/java/com/selfrunner/apimodule/global/aop/Pointcuts.java
+++ api-module/src/main/java/com/selfrunner/apimodule/global/aop/Pointcuts.java
@@ -4,8 +4,9 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class Pointcuts {
 
-    @Pointcut("execution(* com.selfrunner.commonmodule.domain.*.service.*.*(..))")
+    // application 패키지 내 모든 *.Service 클래스의 모든 메서드를 포인트컷으로 지정
+    @Pointcut("execution(* com.selfrunner.apimodule.application..*Service.*(..))")
     public void allService() {
-
+        // Pointcut signature
     }
 }
