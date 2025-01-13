9c4354beb011542f87f62a616c73006f8e4fd5a7
Jeongjin Lee <sso07012@mail.hongik.ac.kr>
2025-01-14 03:00:18 +0900

    Hotfix: 과릿 1.4.3 (#487)
    * Hotfix: application.yml 오류 수정 (#482)
    
    * Hotfix: Lecture 생성 오류 수정 (#482)
    
    * Hotfix: Lecture 생성 오류 수정 (#482)
    
    * Hotfix: Lecture 생성 오류 수정 (#482)
    
    * Hotfix: Lecture 생성 오류 수정 (#482)

--- common-module/src/main/java/com/selfrunner/commonmodule/common/Day.java
+++ common-module/src/main/java/com/selfrunner/commonmodule/common/Day.java
@@ -6,17 +6,12 @@ import lombok.ToString;
 @Getter
 @ToString
 public enum Day {
-    MON("월요일"),
-    TUE("화요일"),
-    WED("수요일"),
-    THU("목요일"),
-    FRI("금요일"),
-    SAT("토요일"),
-    SUN("일요일");
+    MON, // "월요일"
+    TUE, // "화요일"
+    WED, // "수요일"
+    THU, // "목요일"
+    FRI, // "금요일"
+    SAT, // "토요일"
+    SUN // "일요일"
 
-    private final String DayName;
-
-    Day(String dayName) {
-        this.DayName = dayName;
-    }
 }
